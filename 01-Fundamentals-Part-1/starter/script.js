47a2d6a2f251881ed86863c75cc65e1b9843790b
Chee Jenn Yeong <cheejennyeong95@gmail.com>
2022-10-11 21:24:20 +0800

    assignment 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,9 @@
 let js = "amazing";
 if (js === "amazing") alert("JavaScript is FUN!");
 console.log(40 + 8 + 23 - 10);
+
+let country = "Singapore";
+let continent = "Asia";
+let population = 5;
+
+console.log(country, continent, population);
