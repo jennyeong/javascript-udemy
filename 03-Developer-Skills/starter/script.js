66052e2654ade7dcabeaef3ce280544927f5078a
Chee Jenn Yeong <cheejennyeong95@gmail.com>
2022-10-12 17:23:01 +0800

    Challenge 9

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// Challenge 9
+const printForecase = function (arr) {
+  let result = "";
+  for (let i = 0; i < arr.length; i++) {
+    result += `... ${arr[i]}°C in ${i + 1} days `;
+  }
+  console.log(result);
+};
+
+printForecase([17, 21, 23]);
+printForecase([12, 5, -5, 0, 4]);
