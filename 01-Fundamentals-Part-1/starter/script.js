cef753186af21151faf6e7a61ee2f250ea195432
Chee Jenn Yeong <cheejennyeong95@gmail.com>
2022-10-12 11:28:54 +0800

    Assignment 9

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -154,3 +154,24 @@ if (
 } else {
   console.log("Round 3 : No teams win...");
 }
+
+// Assignment 9
+switch (language) {
+  case "chinese" || "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "English":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+}
