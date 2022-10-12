9f09d278e5f6a9cb8a199260cdc596e14cea9de4
Chee Jenn Yeong <cheejennyeong95@gmail.com>
2022-10-12 15:30:01 +0800

    Assignment 18

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -180,3 +180,16 @@ console.log(
         }'s (${mark.calcBMI()})!`
   }`
 );
+
+// Assignment 18
+myCountry.describe = function () {
+  console.log(
+    `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`
+  );
+};
+myCountry.describe();
+myCountry.checkIsland = function () {
+  this.isIsland = this.neighbours.length === 0 ? true : false;
+};
+myCountry.checkIsland();
+console.log(myCountry.isIsland);
