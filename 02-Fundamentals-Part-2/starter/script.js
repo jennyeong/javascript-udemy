ecb95b9accd883c81a5cd8c59a0ded569905a5d0
Chee Jenn Yeong <cheejennyeong95@gmail.com>
2022-10-12 15:21:27 +0800

    Challenge 7

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -147,3 +147,36 @@ console.log(
 
 console.log(myCountry.population + 2);
 console.log(myCountry["population"] - 2);
+
+// Challenge 7
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    const bmi = this.mass / (this.height * this.height);
+    return bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    const bmi = this.mass / (this.height * this.height);
+    return bmi;
+  },
+};
+
+console.log(
+  `${
+    mark.calcBMI() > john.calcBMI()
+      ? `${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${
+          john.fullName
+        }'s (${john.calcBMI()})!`
+      : `${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${
+          mark.fullName
+        }'s (${mark.calcBMI()})!`
+  }`
+);
